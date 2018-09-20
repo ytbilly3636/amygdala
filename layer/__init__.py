32dfe494f8b1a19de7858a5f5723e9b1d2d4afcf
Yuichiro Tanaka <tanaka.yuichiro483@mail.kyutech.jp>
2018-09-20 07:55:20 +0000

    remove layer folder

--- layer/__init__.py
+++
@@ -1,7 +0,0 @@
-from la import LateralNucleus
-from ce import CentralNucleus
-
-from la_uint import LateralNucleusUint
-from ce_uint import CentralNucleusUint
-
-from la_rc import LateralNucleusRecurrent
--- layer/ce_uint.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from ce import CentralNucleus
-
-class CentralNucleusUint(CentralNucleus):
-    def __init__(self):
-        pass
